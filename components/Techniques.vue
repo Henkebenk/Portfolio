a1fc80bb0518cf1023fafae141f69f9f13b89774
henrik <henha806@student.liu.se>
2025-04-08 15:49:48 +0200

    Added hover indication for techniques, removed some techniques

--- components/Techniques.vue
+++ components/Techniques.vue
@@ -1,79 +1,92 @@
 <template>
     <div class="w-full flex flex-col gap-2">
       <h1 class="text-3xl font-bold">Current technologies</h1>
-      <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2">
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
-          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #41B8833A;">
+      <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 transition-all duration-100">
+        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2 border-2 border-surface-100 border-solid hover:border-surface-200">
+          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #41B88349;">
             <img src="../public/icons/vue.svg" class="w-6 h-6">
           </div>
           <label>Vue</label>
         </div>
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
+        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2 border-2 border-surface-100 border-solid hover:border-surface-200">
           <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #44A8B32E;">
             <img src="../public/icons/tailwind.svg" class="w-6 h-6">
           </div>
           <label>Tailwind</label>
         </div>
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
-          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #6CCEF24B;">
-            <img src="../public/icons/flutter.svg" class="w-6 h-6">
-          </div>
-          <label>Flutter</label>
-        </div>
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
-          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #FF910042;">
-            <img src="../public/icons/firebase.svg" class="w-6 h-6">
-          </div>
-          <label>Firebase</label>
-        </div>
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
-          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #2395ED2A;">
-            <img src="../public/icons/docker.svg" class="w-6 h-6">
-          </div>
-          <label>Docker</label>
-        </div>
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
-          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #8CC84B2B;">
-            <img src="../public/icons/node.svg" class="w-6 h-6">
-          </div>
-          <label>Node</label>
-        </div>
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
-          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #0000001E;">
-            <img src="../public/icons/next.svg" class="w-6 h-6">
-          </div>
-          <label>NextJS</label>
-        </div>
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
-          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #904BC825;">
-            <img src="../public/icons/figma.svg" class="w-6 h-6">
-          </div>
-          <label>Figma</label>
-        </div>
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
-          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #EE5D283B;">
-            <img src="../public/icons/html.svg" class="w-6 h-6">
-          </div>
-          <label>HTML</label>
-        </div>
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
-          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #33AADD30;">
-            <img src="../public/icons/css.svg" class="w-6 h-6">
-          </div>
-          <label>CSS</label>
-        </div>
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
+        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2 border-2 border-surface-100 border-solid hover:border-surface-200">
           <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #F7DE1E57;">
             <img src="../public/icons/javascript.svg" class="w-6 h-6">
           </div>
           <label>Javascript</label>
         </div>
-        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
+        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2 border-2 border-surface-100 border-solid hover:border-surface-200">
+          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #8CC84B2B;">
+            <img src="../public/icons/node.svg" class="w-6 h-6">
+          </div>
+          <label>Node</label>
+        </div>
+        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2 border-2 border-surface-100 border-solid hover:border-surface-200">
+          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #FF910042;">
+            <img src="../public/icons/firebase.svg" class="w-6 h-6">
+          </div>
+          <label>Firebase</label>
+        </div>
+        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2 border-2 border-surface-100 border-solid hover:border-surface-200">
           <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #3ECF8E43;">
             <img src="../public/icons/supabase.svg" class="w-6 h-6">
           </div>
           <label>Supabase</label>
         </div>
+        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2 border-2 border-surface-100 border-solid hover:border-surface-200">
+          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #904BC825;">
+            <img src="../public/icons/figma.svg" class="w-6 h-6">
+          </div>
+          <label>Figma</label>
+        </div>
+        <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2 border-2 border-surface-100 border-solid hover:border-surface-200">
+          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #0000001E;">
+            <img src="../public/icons/expo.svg" class="w-6 h-6">
+          </div>
+          <label>Expo</label>
+        </div>
+        <!-- <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
+          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #6CCEF24B;">
+            <img src="../public/icons/flutter.svg" class="w-6 h-6">
+          </div>
+          <label>Flutter</label>
+        </div> -->
+        <!-- <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
+          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #2395ED2A;">
+            <img src="../public/icons/docker.svg" class="w-6 h-6">
+          </div>
+          <label>Docker</label>
+        </div> -->
+        <!-- <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
+          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #0000001E;">
+            <img src="../public/icons/next.svg" class="w-6 h-6">
+          </div>
+          <label>NextJS</label>
+        </div> -->
+
+        <!-- <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
+          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #EE5D283B;">
+            <img src="../public/icons/html.svg" class="w-6 h-6">
+          </div>
+          <label>HTML</label>
+        </div> -->
+        <!-- <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
+          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #33AADD30;">
+            <img src="../public/icons/css.svg" class="w-6 h-6">
+          </div>
+          <label>CSS</label>
+        </div> -->
+        <!-- <div class="bg-surface-100 p-2 rounded-lg flex items-center gap-2">
+          <div class="w-9 h-9 flex justify-center items-center rounded-md" style="background-color: #61DCFB34;">
+            <img src="../public/icons/react_native.svg" class="w-6 h-6">
+          </div>
+          <label>React native</label>
+        </div> -->
       </div>
     </div>
   </template>

7099c5cb5b92221f292f1e35bd2523770c9b8d9d
henrik <henha806@student.liu.se>
2025-04-25 10:24:43 +0200

    Added new project card, updated favicon

---
+++ components/ProjectCardLarge.vue
@@ -0,0 +1,42 @@
+<template>
+    <UCard>
+        <div class="flex flex-col gap-4 sm:gap-6">
+            <div class="flex flex-col gap-6 justify-between h-full">
+                <div>
+                    <h3
+                        class="text-lg text-pretty tracking-tight font-semibold"
+                    >
+                        {{ props.title }}
+                    </h3>
+                    <p class="text-dimmed line-clamp-2">
+                        {{ props.short_description }}
+                    </p>
+                </div>
+                <div class="bg-muted rounded-lg w-full aspect-[3/2]">
+                    <img
+                        :src="props.image_url"
+                        class="object-cover w-full h-full"
+                    />
+                </div>
+            </div>
+            <div class="flex gap-1.5">
+                <UBadge
+                    color="neutral"
+                    variant="soft"
+                    class="text-muted"
+                    v-for="technology in technologies"
+                    >{{ technology }}</UBadge
+                >
+            </div>
+        </div>
+    </UCard>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    title: { type: String, required: true },
+    image_url: { type: String, required: true },
+    short_description: { type: String, required: true },
+    technologies: { type: Array, required: true },
+});
+</script>
---
+++ components/SocialsButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <UTooltip :text="props.title">
+            <UButton
+                :icon="props.icon"
+                color="neutral"
+                variant="ghost"
+                :to="props.url"
+                class="cursor-pointer"
+                size="xl"
+            />
+        </UTooltip>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    title: { type: String, required: true },
+    url: { type: String, required: true },
+    icon: { type: String, required: true },
+});
+</script>

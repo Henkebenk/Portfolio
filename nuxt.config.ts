6c163aa6d280483b453bfe616f447e0135530973
Henrik <henkeharshagen@gmail.com>
2025-10-09 15:51:48 +0200

    Compressed image, faster loading

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxtjs/supabase', '@nuxtjs/mdc'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxtjs/supabase',
+    '@nuxtjs/mdc',
+    '@nuxt/image'
+  ],
 
   devtools: {
     enabled: true
